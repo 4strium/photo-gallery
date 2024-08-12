2d8d67ce8e935709fcc30120882c36b151d14aa8
Romain M. <88449922+4strium@users.noreply.github.com>
2024-08-12 13:17:43 +0200

    Add swiper for images in landing page

---
+++ static/js/app.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  InitialSlide: 2,
+  speed : 600,
+  preventClicks: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  on: {
+    click(event){
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+});
